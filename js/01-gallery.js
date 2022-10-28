4223815254dcfac2bcb9ef34b5e4787d8533d3f0
dmitrij-pularija <dmitrij.pularija@gmail.com>
2022-10-28 17:53:12 +0300

    hw8

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -33,7 +33,7 @@ const lightbox = new SimpleLightbox(".gallery__link", {
   captionsData: "alt",
   captionPosition: "bottom",
   captionDelay: 250,
-  close: false,
+  close: false
 });
 
 function viewImg(event) {
